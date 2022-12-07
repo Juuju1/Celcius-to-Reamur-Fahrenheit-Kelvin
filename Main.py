61cdc1b67b02e074e8df9733ca0e90e17ea7fdc6
Juuu <110470422+Juujuts@users.noreply.github.com>
2022-12-07 23:23:05 +0700

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,14 @@
+print("-PROGRAM CELCIUS-")
+
+celcius = float (input('Masukan suhu dalam Celcius : '))
+print("Suhu adalah", celcius, "Celcius")
+
+reamur = (4/5) * celcius
+print("Suhu dalam Reamur adalah", reamur, "Reamur")
+
+fahrenheit = ((9/5) * celcius ) + 32
+print("Suhu dalam Fahrenhet adalah", fahrenheit, "Fahrenheit")
+
+kelvin = celcius + 273
+print("Suhu dalam Kelvin adalah ", kelvin, "Kelvin")
+
